ebf5850939587d4ddf64feaf6621601eaebefa24
8dspaces <mickqi1985@hotmail.com>
2014-11-06 21:27:24 +0800

    Update 24_flat_list.py

--- checkio/24_flat_list.py
+++ checkio/24_flat_list.py
@@ -1,6 +1,28 @@
-def flat_list_0(data):
-    return map(int, str(data).replace('[','').replace(']','').replace(' ', '').split(','))
-
+def flat_list_2(data):
+    result = []
+    def flat(data):
+        while data:
+            i = data.pop(0)
+            if not isinstance(i, iters):
+                result.append(i)
+            else:
+                flat(i)
+    flat(data)
+    return result
+    
+def flat_list_3(data):
+    r=[]
+    while data:
+        i = data.pop(0)
+        if not isinstance(i, list):
+            r.append(i)
+        else:
+            flat_list(i, r)
+    return r  
+    
+def flat_list(d):
+    d = str(d).replace('[','').replace(']','').replace(',',' ').split()
+    return map(int, d) if d else []   
 
 def flat_list_1(a):
     t = []
@@ -10,13 +32,24 @@ def flat_list_1(a):
         else:
             t.append(i)
     return t   
-    
-flat_list=lambda L: sum(map(flat_list,L),[]) if isinstance(L,list) else [L]
+# reference     
+flat_list_5=lambda L: sum(map(flat_list,L),[]) if isinstance(L,list) else [L]
 
+def flat_list_4(l):
+    r = []
+    def f(l):
+        for i in l:
+            r.append(i) if type(i) is int else f(i)
+    f(l)
+    return r
 print flat_list([1, 2, 3]) == [1, 2, 3]
 print flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4]
 print flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7]
 print flat_list([-1, [1, [-2], 1], -1]) == [-1,1,-2,1,-1]
+print flat_list([-1,[1,[-2,[3],[[5],[10,-11],[1,100,[-1000,[5000]]],[20,-10,[[[]]]]]]]])
+print flat_list([])   
+
+
 
 # one level list 
 import itertools
