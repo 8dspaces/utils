b23259f3db7c9ccd505a53ad130a649aa4d5a963
8dspaces <mickqi1985@hotmail.com>
2014-11-06 15:53:06 +0800

    Update 24_flat_list.py

--- checkio/24_flat_list.py
+++ checkio/24_flat_list.py
@@ -1,4 +1,4 @@
-def flat_list(data):
+def flat_list_0(data):
     return map(int, str(data).replace('[','').replace(']','').replace(' ', '').split(','))
 
 
@@ -9,10 +9,17 @@ def flat_list_1(a):
             t += flat_list(i)
         else:
             t.append(i)
-    return t     
-
+    return t   
+    
+flat_list=lambda L: sum(map(flat_list,L),[]) if isinstance(L,list) else [L]
 
 print flat_list([1, 2, 3]) == [1, 2, 3]
 print flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4]
 print flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7]
 print flat_list([-1, [1, [-2], 1], -1]) == [-1,1,-2,1,-1]
+
+# one level list 
+import itertools
+list2d = [[1,2,3],[4,5,6], [7], [8,9]]
+merged = list(itertools.chain(*list2d))
+print merged
